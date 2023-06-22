c01ab68366c6b5ecde0c01d0f34748051c076695
lioramael <liora.mael@colorado.edu>
2023-06-22 10:03:50 -0600

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# Repo for in-lab experimentation for CASA research. 
+Authors: Thomas Dunnington, Liora Mael, Nicholas Gotlib
---
+++ TwoSMPSCalibration/correctionConcAged.m
@@ -0,0 +1,49 @@
+%% Clean Up
+close all; clear; clc;
+
+%% Import Data
+lowInd = 21;
+highInd = 87;
+
+%Date from my aged experiments
+load('correctionsAged.mat');
+
+%Calculate the average, std, and +-10%
+allTogether = [concRatios0509, concRatios0512, concRatios0516, concRatios0519, concRatios0522, concRatios0524];
+avgAged = mean(allTogether, 2);
+stdDev = std(allTogether, 0, 2);
+
+figure();
+% lowRH = plot(bins(lowInd:highInd), concRatios0516(lowInd:highInd), 'r');
+hold on;
+% plot(bins(lowInd:highInd), concRatios0509(lowInd:highInd), 'r');
+% plot(bins(lowInd:highInd), concRatios0512(lowInd:highInd), 'r');
+% plot(bins(lowInd:highInd), concRatios0519(lowInd:highInd), 'g');
+% plot(bins(lowInd:highInd), concRatios0522(lowInd:highInd), 'g');
+plot(bins(lowInd:highInd), concRatios0602(lowInd:highInd), 'b');
+fresh = plot(bins(lowInd:highInd), concRatios0605(lowInd:highInd), 'b');
+plot(bins(lowInd:highInd), concRatios0606(lowInd:highInd), 'b');
+plot(bins(lowInd:highInd), concRatios0531(lowInd:highInd), 'b');
+% medRH = plot(bins(lowInd:highInd), concRatios0524(lowInd:highInd), 'g');
+Freshq = plot(bins(lowInd:highInd), concRatios0506(lowInd:highInd), 'm');
+plot(bins(lowInd:highInd), concRatios0508(lowInd:highInd), 'm');
+plot(bins(lowInd:highInd), concRatios0514(lowInd:highInd), 'm');
+% avgplot = plot(bins(lowInd:highInd), avgAged(lowInd:highInd), 'k', 'linewidth', 2);
+
+%Error bars and standard deviation
+% plot(bins(lowInd:highInd), avgAged(lowInd:highInd) + stdDev(lowInd:highInd), 'b');
+% plot(bins(lowInd:highInd), avgAged(lowInd:highInd) - stdDev(lowInd:highInd), 'b');
+% plot(bins(lowInd:highInd), 1.1*avgAged(lowInd:highInd), 'm');
+% plot(bins(lowInd:highInd), 0.9*avgAged(lowInd:highInd), 'm');
+
+%Plotting labels
+xlabel('Bin (nm)');
+ylabel('Concentration Correction factor (butanol/water)');
+title('SMPS Correction Factor');
+legend([lowRH, medRH, fresh, Freshq, avgplot], 'Low RH Aged', 'Med RH Aged', 'Fresh New', 'Fresh Early', 'Avg','location', 'nw');
+
+%Saving the correction data
+save('finalCorrection.mat', 'avgAged');
+
+
+
